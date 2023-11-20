db92b3ce833d002b2e3f0cdc8b4b7850907d5643
laure devillers <devlaure79@gmail.com>
2023-11-20 16:19:20 +0100

    validations ajoutées

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,6 +6,11 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   # validations
-  validates :model, :year, :color, :fuel, :price, :description, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :color, presence: true
+  validates :fuel, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
   validates :fuel, inclusion: { in: FUEL, message: "%{value} is not a valid fuel type" }
 end
