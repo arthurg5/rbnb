48afd440fefd35e8c18e3c17740cbaa7d4d9613f
laure devillers <devlaure79@gmail.com>
2023-11-20 16:11:06 +0100

    validations dans le model car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,4 @@
 class CarsController < ApplicationController
+
 end
+
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,11 @@
 class Car < ApplicationRecord
+  # authentification
+  skip_before_action :authenticate_user!, only: :home
+  # fuel choices
+  FUEL = %w(Diesel Gasoline Biofuels LPG Electric Ethanol Hybrid)
   belongs_to :user
   has_many :bookings
+  # validations
+  validates :model, :year, :color, :fuel, :price, :description, presence: true
+  validates :fuel, inclusion: { in: FUEL, message: "%{value} is not a valid fuel type" }
 end
