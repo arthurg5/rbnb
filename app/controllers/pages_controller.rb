575c71e346d7c37e3d3f43bb3ef6531f4fcd24f0
Lucile Devilla <ludevil.coding@gmail.com>
2023-11-20 15:10:49 +0100

    dashboard views added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-
   def home
   end
 
   def dashboard
+    # @user = User.find(params[:user_id])
+    # @booking.user = @user
+    # @car.user = @user
   end
 end
