c2f7e887202706c92dac3ae25958a10bc8cf9495
laure devillers <devlaure79@gmail.com>
2023-11-20 16:56:43 +0100

    fin création du controller et modèle car avec tests

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ApplicationRecord
   # authentification
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
   # fuel choices
   FUEL = %w(Diesel Gasoline Biofuels LPG Electric Ethanol Hybrid)
   belongs_to :user
