a53c34deaa24f1cdd95bf81ded3383054f0655b4
Abdeslam El Mouktafi <abdeslam.elmouktafi@hotmail.fr>
2023-11-22 15:30:53 +0100

    modif footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_111809) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_100919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
