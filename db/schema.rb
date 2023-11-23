d32032409ff36cafd43e4976ec760481170d23eb
Lucile Devilla <ludevil.coding@gmail.com>
2023-11-23 12:20:15 +0100

    autocomplete banner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_153328) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_095527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_153328) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
